511397ee703f0119effb9bf20998e5daa265e276
cougz <github@s.seiffert.me>
2026-05-07 09:48:00 +0200

    fix: allow Cloudflare beacon in CSP, use define:vars for email stats data

--- workers/frontend/src/middleware/index.ts
+++ workers/frontend/src/middleware/index.ts
@@ -15,7 +15,7 @@ export const onRequest: MiddlewareHandler = async (context, next) => {
       "default-src 'self'",
       "font-src 'self' https://fonts.gstatic.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-      "script-src 'self' 'unsafe-inline'",
+      "script-src 'self' 'unsafe-inline' https://static.cloudflareinsights.com",
       "connect-src 'self'",
       "img-src 'self' data:",
       "frame-ancestors 'none'",
